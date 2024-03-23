ae3d25c6d91f12d05ef81a35faccc3d7340c4d28
Ching367436 <44335786+Ching367436@users.noreply.github.com>
2024-03-24 04:33:17 +0800

    Refactor contract initialization and add game contract factory

--- battle-ship/src/util/contractInit.ts
+++ battle-ship/src/util/contractInit.ts
@@ -3,7 +3,8 @@ import { ethers } from "ethers";
 // import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 
 const provider = new ethers.providers.Web3Provider((window as any).ethereum);
-let contract: ethers.Contract;
+let factoryContract: ethers.Contract;
+let gameContract: ethers.Contract;
 
 export async function connectWallet() {
   await provider.send("eth_requestAccounts", []);
@@ -12,10 +13,19 @@ export async function connectWallet() {
 
 export async function initBattleShipGameFactoryContract() {
   const address = '0x5be371B58Ebf06B7D3c08137fe4A4053EAe96581';
-  contract = new ethers.Contract(address, BattleShipGameFactoryAbi, provider);
-  return contract;
+  factoryContract = new ethers.Contract(address, BattleShipGameFactoryAbi, provider);
+  return factoryContract;
 }
 
-export async function getContract() {
-  return contract;
+export async function initBattleShipGameContract(_playersAddress: string[]) {
+  gameContract = await factoryContract.createBattleShipGame(_playersAddress);
+  return gameContract;
+}
+
+export async function getFactoryContract() {
+  return factoryContract;
+}
+
+export async function getBattleShipGameContract() {
+  return gameContract;
 }
