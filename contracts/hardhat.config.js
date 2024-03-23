54180f04947572a7643856333cd6f534370657ee
Ching367436 <44335786+Ching367436@users.noreply.github.com>
2024-03-23 22:42:14 +0800

    hardhat: add typechain plugin

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@typechain/hardhat')
+require('@nomicfoundation/hardhat-ethers')
+require('@nomicfoundation/hardhat-chai-matchers')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false // defaults to false
+  },
 };
